3cdea5e792b2024c98d2ad24da9a8b195ceb9b59
AhmedDip <ahmedrb.dip@gmail.com>
2025-08-09 01:37:07 +0600

    Update README.md File

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UsersTableSeeder;
+use Database\Seeders\RolesAndPermissionsSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call(
+            RolesAndPermissionsSeeder::class,
+            UsersTableSeeder::class
+        );
     }
 }
